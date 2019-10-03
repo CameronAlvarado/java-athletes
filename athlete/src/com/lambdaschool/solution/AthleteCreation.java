9ebec8a05b36e73afc02b8ebdfce8876e627e393
Cameron Alvarado <cameronalvarado@Camerons-Macintosh.local>
2019-10-03 15:43:13 -0700

    Got baseball player to display

---
+++ athlete/src/com/lambdaschool/solution/AthleteCreation.java
@@ -0,0 +1,7 @@
+package src.com.lambdaschool.solution;
+
+public interface AthleteCreation
+{
+    void displayAthlete(/*String sport, String athlete*/);
+    String readMessage();
+}
--- athlete/src/com/lambdaschool/solution/Main.java
+++ athlete/src/com/lambdaschool/solution/Main.java
@@ -1,4 +1,4 @@
-package com.lambdaschool.solution;
+package src.com.lambdaschool.solution;
 
 public class Main
 {
@@ -8,20 +8,19 @@ public class Main
         Processor app;
 
         // Create a Track Athlete
-        injector = new TrackAthleteCreationInjector();
-        app = injector.getProcess();
-        app.displayAthlete();
-
-        // Create a Hockey Athlete
-        injector = new HockeyAthleteCreationInjector();
-        app = injector.getProcess();
-        app.displayAthlete();
-
-
-        // Create a Rugby Athlete
-        injector = new RugbyAthleteCreationInjector();
-        app = injector.getProcess();
-        app.displayAthlete();
+//        injector = new TrackAthleteCreationInjector();
+//        app = injector.getProcess();
+//        app.displayAthlete();
+//
+//        // Create a Hockey Athlete
+//        injector = new HockeyAthleteCreationInjector();
+//        app = injector.getProcess();
+//        app.displayAthlete();
+//
+//        // Create a Rugby Athlete
+//        injector = new RugbyAthleteCreationInjector();
+//        app = injector.getProcess();
+//        app.displayAthlete();
 
         // Create a Baseball Athlete
         injector = new BaseballAthleteCreationInjector();
